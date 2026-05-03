db776157f546f6dc9be2ae5bdcda57f397387aad
MatiasGalliani <matiasgalliani00@gmail.com>
2026-05-03 02:28:02 -0300

    Added logic for not printing stuff when the first age inserted was 0. Change the conditional loop to an infinite loop that breaks when the condition age == 0 is met.

--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
 import "fmt"
-import "os"
 
 func main(){
 	var name, olderWom string
-	var gender, civilStatus, age, ageIndex int
-	var marrOld30, marrMen, men, women int
+	var gender, civilStatus, age, womAgeIndex int
+	var marrOld30, marrMen, men, women, closingIndex int
 	var singleMen, womSingYoung25, sumAgeMarrMen, womMaxAge int
-	var womAgeIndex int
 	var aveAgeMarrMen float64
 
 	womAgeIndex = 0
-	ageIndex = 0
+	closingIndex = 0
 
-	fmt.Print("Insert your age: ")
-	fmt.Scan(&age)
-	if age == 0 {
-		fmt.Println("Age inserted was 0, closing app..")
-		os.Exit(0)
-	}
-
-	for age > 0 {
-		if ageIndex >= 1 {
-			fmt.Print("Insert your age: ")
-			fmt.Scan(&age)
-		}
+	for {
+		fmt.Print("Insert your age (0 to exit): ")
+		fmt.Scan(&age)
 
 		if age == 0 {
 			fmt.Println("Age inserted was 0, closing app...")
@@ -69,18 +58,20 @@ func main(){
 				}
 			}
 		}
-	ageIndex++
+	closingIndex++
 }
 
 	if marrMen > 0 {
 		aveAgeMarrMen = float64(sumAgeMarrMen) / float64(marrMen)
 		fmt.Printf("The average age of the married men is: %.2f\n", aveAgeMarrMen)
 	}
-	
-	fmt.Printf("The amount of married men older than 30 is: %d\n", marrOld30)
-	fmt.Printf("The amount of single women younger than 25 is: %d\n", womSingYoung25)
-	fmt.Printf("The total amount of men is: %d, and the women is: %d\n", men, women)
-	fmt.Printf("The amount of single men is: %d\n", singleMen)
-	fmt.Printf("The oldest women is: %s\n", olderWom)
+
+	if closingIndex > 0 {
+		fmt.Printf("The amount of married men older than 30 is: %d\n", marrOld30)
+		fmt.Printf("The amount of single women younger than 25 is: %d\n", womSingYoung25)
+		fmt.Printf("The total amount of men is: %d, and the women is: %d\n", men, women)
+		fmt.Printf("The amount of single men is: %d\n", singleMen)
+		fmt.Printf("The oldest women is: %s\n", olderWom)	
+	}	
 }
 
